9901a6bbdd56074a21f9ec0bfe37d4c6bcc6d137
Vieirbat <danielemaileletronico@gmail.com>
2022-07-12 23:58:46 -0300

    adicionando clusterização ao painel e novas técnicas de redução de dimensionalidade

--- notebooks/text.py
+++ notebooks/text.py
@@ -17,6 +17,12 @@ import random
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans, MiniBatchKMeans, AgglomerativeClustering, DBSCAN, OPTICS
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
+from umap import UMAP
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
 
 from graph.pyvis.network import Network
 
@@ -374,11 +380,11 @@ class text_mining(object):
             article_file_path = str(row['file'])
             
             title_html = f"""Article Title:{article_title}
-                            Article Date:{article_date}
-                            Article Number Authors:{article_number_authors}
-                            Article Number Citations:{article_number_citations}
-                            Article DOI:{article_doi}
-                            Article File Name:{article_file_name}"""
+                             Article Date:{article_date}
+                             Article Number Authors:{article_number_authors}
+                             Article Number Citations:{article_number_citations}
+                             Article DOI:{article_doi}
+                             Article File Name:{article_file_name}"""
             
             graph.add_node(n_id=article_id, 
                         label=f"Node ID: {str(article_id)[0:4]}", 
@@ -462,6 +468,110 @@ class text_mining(object):
         
         return graph, path_graph_final, os.path.join(path_graph, folder_graph)
     
+    def make_clustering(self,
+                        X,
+                        metric_func=np.mean,
+                        lim_sup=None, 
+                        init='k-means++', 
+                        n_init=10, 
+                        max_iter=30, 
+                        tol=1e-4, 
+                        random_state=0):
+        
+        lim_sup = range(2,int(X.shape[0]**0.5)) if lim_sup == None else lim_sup
+        list_result = []
+
+        for c in lim_sup:
+            
+            objGroup = KMeans(n_clusters=c,
+                            init=init,
+                            n_init=n_init,
+                            max_iter=max_iter,
+                            tol=tol, 
+                            random_state=random_state)
+            
+            objGroup = objGroup.fit(X)
+            
+            inertia = objGroup.inertia_
+            s = silhouette_score(X, objGroup.labels_, metric='euclidean', random_state=random_state)
+            ch = calinski_harabasz_score(X, objGroup.labels_)
+            db = davies_bouldin_score(X, objGroup.labels_)
+            
+            list_result.append({'cluster':c,
+                                'inertia':inertia,
+                                'silhouette':s,
+                                'calinski_harabasz':ch,
+                                'davies_bouldin':db})
+
+        df_metrics = pd.DataFrame(list_result)
+
+        ss = df_metrics.nlargest(1,'silhouette')['cluster'].iat[0]
+        ch = df_metrics.nlargest(1,'calinski_harabasz')['cluster'].iat[0]
+        db = df_metrics.nsmallest(1,'davies_bouldin')['cluster'].iat[0]
+
+        final_cluster_value = int(metric_func([ss, ch, db]))
+
+        objGroup = KMeans(n_clusters=final_cluster_value,
+                        init='k-means++',
+                        n_init=10,
+                        max_iter=30,
+                        tol=1e-4, 
+                        random_state=0)
+
+        objGroup = objGroup.fit(X)
+        
+        return objGroup.labels_
+    
+    def reduce_dimensionality(self, X, y=None, n_components=3, algorithm='UMAP'):
+        
+        """This function get the X data and reduce dimensionality to n_components.
+        
+        algorithm: UMAP, TSNE, PCA
+        
+        """
+
+        if algorithm=="TSNE":
+            objTSNE = TSNE(n_components=n_components, init='random')
+            X_reduce = objTSNE.fit_transform(X)
+        elif algorithm=="PCA":
+            objPCA = PCA(n_components=n_components, random_state =0)
+            X_reduce = objPCA.fit_transform(X)
+        elif algorithm=="MDS":
+            objMDS = MDS(n_components=n_components)
+            X_reduce = objMDS.fit_transform(X)
+        else:
+            # Configure UMAP hyperparameters
+            reducer = UMAP(n_neighbors=100, # default 15, The size of local neighborhood (in terms of number of neighboring sample points) used for manifold approximation.
+                        n_components=n_components, # default 2, The dimension of the space to embed into.
+                        metric='euclidean', # default 'euclidean', The metric to use to compute distances in high dimensional space.
+                        n_epochs=1000, # default None, The number of training epochs to be used in optimizing the low dimensional embedding. Larger values result in more accurate embeddings. 
+                        learning_rate=1.0, # default 1.0, The initial learning rate for the embedding optimization.
+                        init='spectral', # default 'spectral', How to initialize the low dimensional embedding. Options are: {'spectral', 'random', A numpy array of initial embedding positions}.
+                        min_dist=0.1, # default 0.1, The effective minimum distance between embedded points.
+                        spread=1.0, # default 1.0, The effective scale of embedded points. In combination with ``min_dist`` this determines how clustered/clumped the embedded points are.
+                        low_memory=False, # default False, For some datasets the nearest neighbor computation can consume a lot of memory. If you find that UMAP is failing due to memory constraints consider setting this option to True.
+                        set_op_mix_ratio=1.0, # default 1.0, The value of this parameter should be between 0.0 and 1.0; a value of 1.0 will use a pure fuzzy union, while 0.0 will use a pure fuzzy intersection.
+                        local_connectivity=1, # default 1, The local connectivity required -- i.e. the number of nearest neighbors that should be assumed to be connected at a local level.
+                        repulsion_strength=1.0, # default 1.0, Weighting applied to negative samples in low dimensional embedding optimization.
+                        negative_sample_rate=5, # default 5, Increasing this value will result in greater repulsive force being applied, greater optimization cost, but slightly more accuracy.
+                        transform_queue_size=4.0, # default 4.0, Larger values will result in slower performance but more accurate nearest neighbor evaluation.
+                        a=None, # default None, More specific parameters controlling the embedding. If None these values are set automatically as determined by ``min_dist`` and ``spread``.
+                        b=None, # default None, More specific parameters controlling the embedding. If None these values are set automatically as determined by ``min_dist`` and ``spread``.
+                        random_state=42, # default: None, If int, random_state is the seed used by the random number generator;
+                        metric_kwds=None, # default None) Arguments to pass on to the metric, such as the ``p`` value for Minkowski distance.
+                        angular_rp_forest=False, # default False, Whether to use an angular random projection forest to initialise the approximate nearest neighbor search.
+                        target_n_neighbors=-1, # default -1, The number of nearest neighbors to use to construct the target simplcial set. If set to -1 use the ``n_neighbors`` value.
+                        #target_metric='categorical', # default 'categorical', The metric used to measure distance for a target array is using supervised dimension reduction. By default this is 'categorical' which will measure distance in terms of whether categories match or are different. 
+                        #target_metric_kwds=None, # dict, default None, Keyword argument to pass to the target metric when performing supervised dimension reduction. If None then no arguments are passed on.
+                        #target_weight=0.5, # default 0.5, weighting factor between data topology and target topology.
+                        transform_seed=42, # default 42, Random seed used for the stochastic aspects of the transform operation.
+                        verbose=False, # default False, Controls verbosity of logging.
+                        unique=False # default False, Controls if the rows of your data should be uniqued before being embedded. 
+                        )
+            
+            X_reduce = reducer.fit_transform(X, y)
+        
+        return X_reduce
 
 
 class text_viz(object):
